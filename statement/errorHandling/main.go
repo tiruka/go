e6a7c151891bfab7976674a4df6ac29e33ed2f38
iruka t <tiruka.engineer@gmail.com>
2020-05-17 17:40:11 +0900

    add error handling, panic

---
+++ statement/errorHandling/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./main.go")
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
+
+	err = os.Chdir("test")
+	if err != nil {
+		log.Fatalln("Error")
+	}
+}
