a94b0425f03d511b7ad2433ed84bc94064353013
iruka t <tiruka.engineer@gmail.com>
2020-05-17 17:24:37 +0900

    add log

---
+++ statement/log/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func LoggingSetting(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
+
+func main() {
+	LoggingSetting("test.log")
+	log.Println("logging!")
+	log.Printf("%T %v", "test", "test")
+	log.Fatalf("%T %v", "test", "test")
+	log.Fatalln("error")
+	log.Println("ok") // not print
+}
