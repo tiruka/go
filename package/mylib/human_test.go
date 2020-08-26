feb1ac38f363729c5c95f0bfdc56cbb0d1aab3f1
iruka t <tiruka.engineer@gmail.com>
2020-08-26 11:36:48 +0900

    godoc

---
+++ package/mylib/human_test.go
@@ -0,0 +1,6 @@
+package mylib
+
+func ExamplePerson_SayHello() {
+	p := Person{"Mike", 20}
+	p.SayHello()
+}
--- package/mylib/math.go
+++ package/mylib/math.go
@@ -1,5 +1,9 @@
+/*
+my special library
+*/
 package mylib
 
+// Average returns the average of a series of numbers
 func Average(s []int) int {
 	total := 0
 	for _, i := range s {
